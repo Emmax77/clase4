ade6175fcafd182af9d8526adf5733e36990d889
Estudiante <Estudiante@1303-23>
2017-02-02 19:43:07 -0600

    subida ultima

--- WindowsApplication1/Form1.Designer.vb
+++ WindowsApplication1/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class Clase4
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
@@ -22,52 +22,127 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnAceptar = New System.Windows.Forms.Button()
+        Me.txtDerecho = New System.Windows.Forms.TextBox()
+        Me.lbTitulo = New System.Windows.Forms.Label()
+        Me.cbxAreas = New System.Windows.Forms.ComboBox()
+        Me.lbDerecho = New System.Windows.Forms.Label()
+        Me.lbIzquierdo = New System.Windows.Forms.Label()
+        Me.txtIzquierdo = New System.Windows.Forms.TextBox()
+        Me.txtRespuesta = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'Button1
+        'btnAceptar
         '
-        Me.Button1.Location = New System.Drawing.Point(64, 80)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnAceptar.Location = New System.Drawing.Point(28, 157)
+        Me.btnAceptar.Name = "btnAceptar"
+        Me.btnAceptar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAceptar.TabIndex = 0
+        Me.btnAceptar.Text = "CALCULAR"
+        Me.btnAceptar.UseVisualStyleBackColor = True
         '
-        'TextBox1
+        'txtDerecho
         '
-        Me.TextBox1.Location = New System.Drawing.Point(64, 46)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 1
+        Me.txtDerecho.Location = New System.Drawing.Point(28, 115)
+        Me.txtDerecho.Name = "txtDerecho"
+        Me.txtDerecho.Size = New System.Drawing.Size(88, 20)
+        Me.txtDerecho.TabIndex = 1
+        Me.txtDerecho.Text = "0"
+        Me.txtDerecho.Visible = False
+        '
+        'lbTitulo
+        '
+        Me.lbTitulo.AutoSize = True
+        Me.lbTitulo.Location = New System.Drawing.Point(25, 18)
+        Me.lbTitulo.Name = "lbTitulo"
+        Me.lbTitulo.Size = New System.Drawing.Size(226, 13)
+        Me.lbTitulo.TabIndex = 2
+        Me.lbTitulo.Text = "Seleccione alguna figura para calcular su area"
+        '
+        'cbxAreas
+        '
+        Me.cbxAreas.FormattingEnabled = True
+        Me.cbxAreas.Items.AddRange(New Object() {"Cuadrado", "Rectangulo", "Triangulo", "Paralelogramo"})
+        Me.cbxAreas.Location = New System.Drawing.Point(28, 43)
+        Me.cbxAreas.Name = "cbxAreas"
+        Me.cbxAreas.Size = New System.Drawing.Size(121, 21)
+        Me.cbxAreas.TabIndex = 3
+        '
+        'lbDerecho
+        '
+        Me.lbDerecho.AutoSize = True
+        Me.lbDerecho.Location = New System.Drawing.Point(28, 84)
+        Me.lbDerecho.Name = "lbDerecho"
+        Me.lbDerecho.Size = New System.Drawing.Size(50, 13)
+        Me.lbDerecho.TabIndex = 4
+        Me.lbDerecho.Text = "Numero1"
+        Me.lbDerecho.Visible = False
+        '
+        'lbIzquierdo
+        '
+        Me.lbIzquierdo.AutoSize = True
+        Me.lbIzquierdo.Location = New System.Drawing.Point(151, 84)
+        Me.lbIzquierdo.Name = "lbIzquierdo"
+        Me.lbIzquierdo.Size = New System.Drawing.Size(50, 13)
+        Me.lbIzquierdo.TabIndex = 5
+        Me.lbIzquierdo.Text = "Numero2"
+        Me.lbIzquierdo.Visible = False
+        '
+        'txtIzquierdo
+        '
+        Me.txtIzquierdo.Location = New System.Drawing.Point(154, 114)
+        Me.txtIzquierdo.Name = "txtIzquierdo"
+        Me.txtIzquierdo.Size = New System.Drawing.Size(100, 20)
+        Me.txtIzquierdo.TabIndex = 6
+        Me.txtIzquierdo.Text = "0"
+        Me.txtIzquierdo.Visible = False
+        '
+        'txtRespuesta
+        '
+        Me.txtRespuesta.Location = New System.Drawing.Point(28, 217)
+        Me.txtRespuesta.Name = "txtRespuesta"
+        Me.txtRespuesta.Size = New System.Drawing.Size(149, 20)
+        Me.txtRespuesta.TabIndex = 7
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(64, 13)
+        Me.Label1.Location = New System.Drawing.Point(25, 201)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
-        Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(152, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "El area de la figura elegida es: "
         '
-        'Form1
+        'Clase4
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 261)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Button1)
-        Me.Name = "Form1"
+        Me.Controls.Add(Me.txtRespuesta)
+        Me.Controls.Add(Me.txtIzquierdo)
+        Me.Controls.Add(Me.lbIzquierdo)
+        Me.Controls.Add(Me.lbDerecho)
+        Me.Controls.Add(Me.cbxAreas)
+        Me.Controls.Add(Me.lbTitulo)
+        Me.Controls.Add(Me.txtDerecho)
+        Me.Controls.Add(Me.btnAceptar)
+        Me.Name = "Clase4"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Button1 As Button
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnAceptar As Button
+    Friend WithEvents txtDerecho As TextBox
+    Friend WithEvents lbTitulo As Label
+    Friend WithEvents cbxAreas As ComboBox
+    Friend WithEvents lbDerecho As Label
+    Friend WithEvents lbIzquierdo As Label
+    Friend WithEvents txtIzquierdo As TextBox
+    Friend WithEvents txtRespuesta As TextBox
     Friend WithEvents Label1 As Label
 End Class
--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -1,3 +1,80 @@
-﻿Public Class Form1
+﻿Public Class Clase4
+    Dim calculo As String
 
+    Private Sub btnAceptar_Click(sender As Object, e As EventArgs) Handles btnAceptar.Click
+
+        Select Case calculo
+            Case "Cuadrado"
+                Dim num As Integer = txtDerecho.Text
+                Dim devuelve As Integer = cuadrado(num)
+                txtRespuesta.Text = devuelve
+            Case "Rectangulo"
+                Dim num As Integer = txtDerecho.Text
+                Dim num2 As Integer = txtIzquierdo.Text
+                Dim devuelve As Integer = rectangulo(num, num2)
+                txtRespuesta.Text = devuelve
+            Case "Triangulo"
+                Dim num As Integer = txtDerecho.Text
+                Dim num2 As Integer = txtIzquierdo.Text
+                Dim devuelve As Integer = rectangulo(num, num2)
+                txtRespuesta.Text = devuelve
+            Case "Paralelogramo"
+                Dim num As Integer = txtDerecho.Text
+                Dim num2 As Integer = txtIzquierdo.Text
+                Dim devuelve As Integer = rectangulo(num, num2)
+                txtRespuesta.Text = devuelve
+            Case ""
+                MessageBox.Show("Debe elegir algun metodo")
+        End Select
+
+    End Sub
+    Function cuadrado(a As Integer)
+        Dim total As Integer = a * a
+        Return total
+    End Function
+
+    Function rectangulo(a As Integer, b As Integer)
+        Dim total As Integer = a * b
+        Return total
+    End Function
+
+    Function triangulo(a As Integer, h As Integer)
+        Dim total As Integer = (a * h) / 2
+        Return total
+    End Function
+
+    Function paralelogramo(a As Integer, h As Integer)
+        Dim total As Integer = a * h
+        Return total
+    End Function
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbxAreas.SelectedIndexChanged
+        Dim numero As String = cbxAreas.SelectedItem
+        Select Case numero
+            Case "Cuadrado"
+                calculo = "Cuadrado"
+                txtDerecho.Visible = True
+                lbDerecho.Visible = True
+                lbIzquierdo.Visible = False
+                txtIzquierdo.Visible = False
+            Case "Rectangulo"
+                calculo = "Rectangulo"
+                txtDerecho.Visible = True
+                txtIzquierdo.Visible = True
+                lbDerecho.Visible = True
+                lbIzquierdo.Visible = True
+            Case "Triangulo"
+                calculo = "Triangulo"
+                txtDerecho.Visible = True
+                txtIzquierdo.Visible = True
+                lbDerecho.Visible = True
+                lbIzquierdo.Visible = True
+            Case "Paralelogramo"
+                calculo = "Paralelogramo"
+                txtDerecho.Visible = True
+                txtIzquierdo.Visible = True
+                lbDerecho.Visible = True
+                lbIzquierdo.Visible = True
+        End Select
+    End Sub
 End Class
